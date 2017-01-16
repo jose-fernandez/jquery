9de73ed9052b3799f90b0e2e616a848cf11a1f84
jose-fernandez <jose.fernandez91@gmail.com>
2017-01-16 10:59:43 +0100

    finished

---
+++ ws3/ej3_9.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#start").click(function(){
+        $("#div1").animate({marginLeft:'0px',backgroundColor: "blue"},1000);
+        $("#div1").animate({opacity:0,marginTop:'10px'},1000);
+    });
+    $('#stop').click(function(){
+    	$('#div1').finish();
+    });
+});
