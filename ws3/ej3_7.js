992628a6119fa03c518dce8cfdee601f096bb426
jose-fernandez <jose.fernandez91@gmail.com>
2017-01-16 01:23:19 +0100

    finished

---
+++ ws3/ej3_7.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('.animated').click(function(){
+		$('#div').animate({
+    		color: "green",
+    		backgroundColor: "brown"
+  		}, 1200);
+  		$('#div').animate({
+    		color: "blue",
+    		backgroundColor: "green"
+  		}, 1200);
+  		$('#div').animate({
+    		color: "red",
+    		backgroundColor: "blue"
+  		}, 1200);
+  		$('#div').animate({
+    		color: "yellow",
+    		backgroundColor: "black"
+  		}, 1200);
+	});
+	$('.nonanimated').click(function(){
+		$('#div').stop();
+	});
+});
